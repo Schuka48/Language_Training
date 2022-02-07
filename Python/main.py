2ff96e7b25f947e076ae6639a2c7c7d18bcf4e41
Schuka48 <artem.shchukin.2000@mail.ru>
2022-02-07 18:06:19 +0300

    Add function that create a person

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,12 @@
-def Hello():
-    print("Hello, World!")
+def create_person(first_name, second_name, age=None) -> dict:
+    person = {}
+    person['first_name'] = first_name
+    person['second_name'] = second_name
+    if age:
+        person['age'] = age
+    return person
 
-Hello()
 
-
+if __name__ == '__main__':
+    print(create_person("Artem", "Schukin", age=21))
 
