a324f4f9db813d3c7ddd263fdae03a79a30966ed
Schuka48 <artem.shchukin.2000@mail.ru>
2022-02-15 19:00:14 +0300

    start learning decorators

--- Python/defmodules.py
+++ Python/defmodules.py
@@ -23,3 +23,37 @@ def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for top in toppings:
         print(f'- {top}')
+
+
+#  Here we go to train decorators
+
+
+def get_bold(func):
+    def wrapped():
+        return "<b>" + func() + "</b>"
+
+    return wrapped
+
+
+def get_italic(func):
+    def wrapped():
+        return "<i>" + func() + "</i>"
+
+    return wrapped
+
+
+@get_italic
+@get_bold
+def get_hello():
+    return "Hello, Artem!"
+
+
+print(get_hello())
+
+
+def getTalk():
+    def innerfunc(letter='Hello'):
+        print(letter, __name__)
+
+    return innerfunc
+
