bf61ef896015651c9157cea856c25846bba2378f
Schuka48 <artem.shchukin.2000@mail.ru>
2022-11-07 21:20:26 +0300

    Create service, that get weather from OpenWeather using APi and whereami

--- Python/Stepik/Module4/test.py
+++ Python/Stepik/Module4/test.py
@@ -1,2 +1,12 @@
-zeros = [0] * 10
-print(zeros)
+n = int(input())
+matrix = [list(map(int, input().split())) for i in range(n)]
+
+
+for i in range(n):
+    for j in range(n):
+        if i != j:
+            if matrix[i][j] != matrix[j][i]:
+                print('NO')
+                exit(1)
+else:
+    print('YES')
